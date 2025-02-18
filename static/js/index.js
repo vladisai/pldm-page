d7c4d30e1bc2347d2285ef9cbfe62b932a804cd6
Vlad Sobal <us441@nyu.edu>
2025-02-18 12:25:23 -0500

    work

--- static/js/index.js
+++ static/js/index.js
@@ -30,32 +30,32 @@ $(document).ready(function() {
     });
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
+            slidesToScroll: 1,
+            slidesToShow: 3,
+            loop: true,
+            infinite: true,
+            autoplay: false,
+            autoplaySpeed: 3000,
     }
 
-		// Initialize all div with carousel class
+        // Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 
     // Loop on each carousel initialized
     for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
+        // Add listener to  event
+        carousels[i].on('before:show', state => {
+            console.log(state);
+        });
     }
 
     // Access to bulmaCarousel instance of an element
     var element = document.querySelector('#my-element');
     if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
+        // bulmaCarousel instance is available as element.bulmaCarousel
+        element.bulmaCarousel.on('before-show', function(state) {
+            console.log(state);
+        });
     }
 
     /*var player = document.getElementById('interpolation-video');
